0027e189deb6b54ba16ab36b3c9315542c3c2379
cgassel <chris.gassel@gmail.com>
2026-07-29 12:57:06 -0500

    Update cache name from v12 to v13

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // connection since they read/write live booking data.
 // ============================================================================
 
-const CACHE_NAME = 'dka-app-v12';
+const CACHE_NAME = 'dka-app-v13';
 const STATIC_ASSETS = [
   './index.html',
   './api.js',
