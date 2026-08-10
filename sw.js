85b86070e8acac0ece6c67f57e7eda88386e57cc
cgassel <chris.gassel@gmail.com>
2026-08-10 13:46:59 -0500

    Update cache name to dka-app-v20

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // connection since they read/write live booking data.
 // ============================================================================
 
-const CACHE_NAME = 'dka-app-v19';
+const CACHE_NAME = 'dka-app-v20';
 const STATIC_ASSETS = [
   './index.html',
   './api.js',
